04334f401d354c48debc74ea4168e91dbd48f1ee
yueyoum <yueyoum@gmail.com>
2015-04-21 16:41:33 +0800

    Fix. don't use absolute path of erl in start_client.sh

--- start_client.sh
+++ start_client.sh
@@ -12,7 +12,7 @@ then
     echo "alreay running"
     exit -1
 else
-    /opt/erlang/bin/erl -pa client/ebin -config client +K true -s make_proxy_client start -detached
+    erl -pa client/ebin -config client +K true -s make_proxy_client start -detached
 fi
 
 exit 0
