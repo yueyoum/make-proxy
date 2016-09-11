e4de2a67367bcbee01d5b79f6924a82682c28364
yueyoum <yueyoum@gmail.com>
2016-09-11 16:34:59 +0800

    Add Socks4 Support

--- client/src/mpc_socks5_child.erl
+++ client/src/mpc_socks5_child.erl
@@ -7,11 +7,11 @@
 
 %% gen_server callbacks
 -export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3]).
 
 -record(state, {key, lsock, socket, remote}).
 
@@ -49,7 +49,7 @@ start_link(LSock) ->
 %%--------------------------------------------------------------------
 init([LSock]) ->
     {ok, Key} = application:get_env(make_proxy_client, key),
-    {ok, #state{key=Key, lsock = LSock}, 0}.
+    {ok, #state{key = Key, lsock = LSock}, 0}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -101,7 +101,7 @@ handle_info(timeout, #state{key = Key, lsock = LSock, socket = undefined} = Stat
         {ok, Remote} ->
             ok = inet:setopts(Socket, [{active, once}]),
             ok = inet:setopts(Remote, [{active, once}]),
-            {noreply, State#state{socket=Socket, remote=Remote}, ?TIMEOUT};
+            {noreply, State#state{socket = Socket, remote = Remote}, ?TIMEOUT};
         {error, Error} ->
             {stop, Error, State}
     end;
@@ -113,7 +113,7 @@ handle_info(timeout, #state{socket = Socket} = State) when is_port(Socket) ->
 
 
 %% recv from client, and send to remote
-handle_info({tcp, Socket, Request}, #state{key=Key, socket=Socket, remote=Remote} = State) ->
+handle_info({tcp, Socket, Request}, #state{key = Key, socket = Socket, remote = Remote} = State) ->
     case gen_tcp:send(Remote, mp_crypto:encrypt(Key, Request)) of
         ok ->
             ok = inet:setopts(Socket, [{active, once}]),
@@ -123,7 +123,7 @@ handle_info({tcp, Socket, Request}, #state{key=Key, socket=Socket, remote=Remote
     end;
 
 %% recv from remote, and send back to client
-handle_info({tcp, Socket, Response}, #state{key=Key, socket=Client, remote=Socket} = State) ->
+handle_info({tcp, Socket, Response}, #state{key = Key, socket = Client, remote = Socket} = State) ->
     {ok, RealData} = mp_crypto:decrypt(Key, Response),
     case gen_tcp:send(Client, RealData) of
         ok ->
@@ -151,7 +151,7 @@ handle_info({tcp_error, _, Reason}, State) ->
 %% @spec terminate(Reason, State) -> void()
 %% @end
 %%--------------------------------------------------------------------
-terminate(_Reason, #state{socket=Socket, remote=Remote}) ->
+terminate(_Reason, #state{socket = Socket, remote = Remote}) ->
     case is_port(Socket) of
         true -> gen_tcp:close(Socket);
         false -> ok
@@ -178,48 +178,86 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 
 -spec(start_process(port(), nonempty_string()) -> {ok, port()} |
-                                                  {error, any()}).
+{error, any()}).
 start_process(Socket, Key) ->
     {ok, RemoteAddr} = application:get_env(make_proxy_client, remote_addr),
     {ok, RemotePort} = application:get_env(make_proxy_client, remote_port),
-    {ok, LocalPort} = application:get_env(make_proxy_client, local_socks5_port),
     {ok, Addr} = inet:getaddr(RemoteAddr, inet),
 
-
-    {ok, Target} = find_target(Socket),
+    {ok, Target, Response} = find_target(Socket),
 
     EncryptedTarget = mp_crypto:encrypt(Key, Target),
     case gen_tcp:connect(Addr, RemotePort, [binary, {active, false}, {packet, 4}]) of
         {ok, RemoteSocket} ->
+            ok = gen_tcp:send(Socket, Response),
             ok = gen_tcp:send(RemoteSocket, EncryptedTarget),
-            ok = gen_tcp:send(Socket, <<5, 0, 0, 1, <<0,0,0,0>>/binary, LocalPort:16>>),
             {ok, RemoteSocket};
         {error, Error} ->
             {error, Error}
     end.
 
-
-%% -spec(find_target(Socket :: port()) -> {ok, <<_:32, _:_*8>>}).
+-spec find_target(port()) -> {ok, binary(), binary()}.
 find_target(Socket) ->
-    {ok, <<5:8, Nmethods:8>>} = gen_tcp:recv(Socket, 2),
-    {ok, _Methods} = gen_tcp:recv(Socket, Nmethods),
+    {ok, <<Ver:8>>} = gen_tcp:recv(Socket, 1),
+    find_target(Socket, Ver).
+
+-spec find_target(port(), integer()) -> {ok, binary(), binary()}.
+find_target(Socket, 4) ->
+    %% http://www.openssh.com/txt/socks4.protocol
+    {ok, <<CD:8>>} = gen_tcp:recv(Socket, 1),
+    case CD =:= 1 orelse CD =:= 2 of
+        true ->
+            ok;
+        false ->
+            CD1 = integer_to_list(CD),
+            erlang:throw(<<"Socket4 Bad CD: ", CD1>>)
+    end,
+
+    {ok, <<Port:16, Address:32>>} = gen_tcp:recv(Socket, 6),
+
+    _UserID = read_socks4_userid(Socket),
+    Target = <<?IPV4, Port:16, Address:32>>,
+    Response = <<0:8, 90:8, Port:16, Address:32>>,
+    {ok, Target, Response};
+
+find_target(Socket, 5) ->
+    %% https://www.ietf.org/rfc/rfc1928.txt
+    {ok, <<NMethods:8>>} = gen_tcp:recv(Socket, 1),
+    {ok, _Methods} = gen_tcp:recv(Socket, NMethods),
 
     gen_tcp:send(Socket, <<5:8/integer, 0:8/integer>>),
     {ok, <<5:8, 1:8, _Rsv:8, AType:8>>} = gen_tcp:recv(Socket, 4),
 
+    Target =
     case AType of
         ?IPV4 ->
             {ok, <<Address:32>>} = gen_tcp:recv(Socket, 4),
             {ok, <<Port:16>>} = gen_tcp:recv(Socket, 2),
-            {ok, <<?IPV4, Port:16, Address:32>>};
+            <<?IPV4, Port:16, Address:32>>;
         ?IPV6 ->
             {ok, <<Address:128>>} = gen_tcp:recv(Socket, 16),
             {ok, <<Port:16>>} = gen_tcp:recv(Socket, 2),
-            {ok, <<?IPV6, Port:16, Address:128>>};
+            <<?IPV6, Port:16, Address:128>>;
         ?DOMAIN ->
             {ok, <<DomainLen:8>>} = gen_tcp:recv(Socket, 1),
             {ok, <<DomainBin/binary>>} = gen_tcp:recv(Socket, DomainLen),
             {ok, <<Port:16>>} = gen_tcp:recv(Socket, 2),
-            {ok, <<?DOMAIN, Port:16, DomainLen:8, DomainBin/binary>>}
-    end.
+            <<?DOMAIN, Port:16, DomainLen:8, DomainBin/binary>>
+    end,
 
+    Response = <<5, 0, 0, 1, <<0, 0, 0, 0>>/binary, 0:16>>,
+    {ok, Target, Response}.
+
+-spec read_socks4_userid(port()) -> binary().
+read_socks4_userid(Socket) ->
+    {ok, <<Part:8>>} = gen_tcp:recv(Socket, 1),
+    read_socks4_userid(Socket, [], Part).
+
+-spec read_socks4_userid(port(), list(), integer()) -> binary().
+read_socks4_userid(_Socket, UserID, 0) ->
+    list_to_binary(lists:reverse(UserID));
+
+read_socks4_userid(Socket, UserID, Part) ->
+    UserID1 = [Part | UserID],
+    {ok, <<Part1:8>>} = gen_tcp:recv(Socket, 1),
+    read_socks4_userid(Socket, UserID1, Part1).
